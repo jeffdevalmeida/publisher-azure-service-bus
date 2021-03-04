9e5c6be88b9a92c4b80cb02a720fdab73ec033cd
Jefferson Almeida <jefferson.almeida@konia.com.br>
2021-03-04 20:39:51 -0300

    primary files

---
+++ FirstApp/Program.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.Azure.ServiceBus;
+using Newtonsoft.Json;
+using System;
+using System.Text;
+
+namespace FirstApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            UserFollowingInputModel inputModel = new UserFollowingInputModel(1, 1, "mail@mail.com");
+            string inputModelJsonString = JsonConvert.SerializeObject(inputModel);
+
+            var messagesBytes = Encoding.UTF8.GetBytes(inputModelJsonString);
+            var message = new Message(messagesBytes);
+
+            SendMessage(message);
+        }
+
+        public static async void SendMessage(Message message)
+        {
+            string busConnectionString = "";
+            string queueName = "example-queue";
+
+            QueueClient queueClient = new QueueClient(busConnectionString, queueName);
+
+            await queueClient.SendAsync(message);
+        }
+    }
+}
---
+++ FirstApp/UserFollowingInputModel.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FirstApp
+{
+    public class UserFollowingInputModel
+    {
+        public UserFollowingInputModel(int idUserFollower, int idUserFollowee, string email)
+        {
+            IdUserFollower = idUserFollower;
+            IdUserFollowee = idUserFollowee;
+            FollowedAt = DateTime.Now;
+            Email = email;
+        }
+
+        public int IdUserFollower { get; private set; }
+        public int IdUserFollowee { get; private set; }
+        public DateTime FollowedAt { get; private set; }
+        public string Email { get; private set; }
+    }
+}
